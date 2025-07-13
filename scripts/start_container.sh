59f2062b3ce09c9c862920959aacab118fb2f5c3
Nookaraju1999 <rajeshraju.yegi@gmail.com>
2025-07-13 02:45:46 -0400

    Update start_container.sh
    This file contains the code,which it pull the docker image from docker hub and run the docker in the specified port

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,1 +1,9 @@
 
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+docker pull nookaraju1999/simple-python-app
+
+# Run the Docker image as a container
+docker run -d -p 5000:5000 nookaraju1999/simple-python-app
